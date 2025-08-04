bcb2724081fc6a6c2f24f77a414ff24e45cec09c
DngDavide02 <dangelodavide02@gmail.com>
2025-08-04 10:52:43 +0200

    Initial commit

---
+++ src/main/java/Team7/Build_Week_T7/BuildWeekT7Application.java
@@ -0,0 +1,13 @@
+package Team7.Build_Week_T7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildWeekT7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildWeekT7Application.class, args);
+	}
+
+}
---
+++ src/test/java/Team7/Build_Week_T7/BuildWeekT7ApplicationTests.java
@@ -0,0 +1,13 @@
+package Team7.Build_Week_T7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildWeekT7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
