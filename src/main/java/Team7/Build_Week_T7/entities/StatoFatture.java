d1291d9923f3b944f3ef49a17e0e81f3c2a5e6be
TagBalu <baluyevslava@icloud.com>
2025-08-06 16:20:02 +0200

    Fatto lo stato fatture controller e service

--- src/main/java/Team7/Build_Week_T7/entities/StatoFatture.java
+++ src/main/java/Team7/Build_Week_T7/entities/StatoFatture.java
@@ -7,13 +7,24 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 public class StatoFatture {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String stato;
+    private String descrizione;
+
+    private boolean attivo=true;
+
+
+    public StatoFatture(String stato,String descrizione) {
+        this.stato = stato;
+        this.descrizione = descrizione;
+    }
 }
